996470e02de9d7ba11d1ca90c7c3fb86480e6940
gerkim62@gmail.com <gerkim62@gmail.com>
2023-01-20 18:19:35 +0300

    bbgfdsshj gr

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import {timetableDetails_form, csvUpload_input, timetableTitle_input, coursesIdentifier_select, timetableContainer_div} from './dom.js'
-import {getCSVStringFrom, cleanCSVString, getCourses, getDays, getTimestamps} from './csvParser.js'
+import { timetableDetails_form, csvUpload_input, timetableTitle_input, coursesIdentifier_select, timetableContainer_div } from './dom.js'
+import { getCSVStringFrom, cleanCSVString, getCourses, getDays, getTimestamps } from './csvParser.js'
 import createTimetable from './table-creator.js'
 
 /*Event listeners*/
@@ -10,23 +10,23 @@ import createTimetable from './table-creator.js'
 @param {Event} e - The submit event
 This function is triggered when the timetableDetails_form is submitted. It gets the files from the csvUpload_input, the value of the timetableTitle_input and the selected value of the coursesIdentifier_select...
 */
-timetableDetails_form.addEventListener('submit', async (e)=>{
+timetableDetails_form.addEventListener('submit', async (e) => {
   e.preventDefault()
-  
+
   const csvFile = csvUpload_input.files[0]
   const timetableTitle = timetableTitle_input.value
   const coursesIdentifier = coursesIdentifier_select.value.trim()
-  
+
   const rawCsvString = await getCSVStringFrom(csvFile)
   const cleanedCSVString = cleanCSVString(rawCsvString)
-  
+
   const courses = getCourses(cleanedCSVString)
-  const timetable_table = createTimetable(courses,timetableTitle, coursesIdentifier)
-  if(timetable_table){
-    timetableContainer_div.setAttribute('hidden','')
+  const timetable_table = createTimetable(courses, timetableTitle, coursesIdentifier)
+  if (timetable_table) {
+    timetableDetails_form.setAttribute('hidden', '')
     timetableContainer_div.removeAttribute('hidden')
-  
-  timetableContainer_div.append(timetable_table)
+
+    timetableContainer_div.append(timetable_table)
   }
-  
+
 })
