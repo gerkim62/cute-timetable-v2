20c8efbc1cd16b8b468b4c3b8be4e0ced8e4d1a0
gerkim62@gmail.com <gerkim62@gmail.com>
2023-02-09 08:58:12 +0300

    gdid

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ function showTimetable({ courses, title }) {
 addEventListener('click',(e)=>{
   const target = e.target
   console.log(target)
-  if(timetableContainer_div.contains(target)||target==propertiesCard_div||propertiesCard_div.contains(target)) return
+  if(target.classList.includes('scheduled')||target==propertiesCard_div||propertiesCard_div.contains(target)) return
   
   hide(propertiesCard_div)
 })
