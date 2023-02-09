a9d83b4151fc281ba9c7914c44f1670b3ffca062
gerkim62@gmail.com <gerkim62@gmail.com>
2023-02-09 23:51:32 +0300

    NF the search functionality

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,7 +6,7 @@ const assetsToCache = [
   "/index.html",
   "/",
   "/pages/offline.html",
-  
+
   "/css/main.css",
   "/css/loader.css",
   "/css/nav.css",
@@ -17,12 +17,15 @@ const assetsToCache = [
   "/css/variables.css",
   "/css/utility.css",
   "/css/spinner.css",
-  
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css',
+
   "/js/main.js",
   "/js/nav.js",
   "/js/functions.js",
   "/js/dom.js"
-  
+
 ];
 
 self.addEventListener("install", event => {
