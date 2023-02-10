e3fad29c59224e515019b16133a333b2a32e2435
gerkim62@gmail.com <gerkim62@gmail.com>
2023-02-10 06:28:49 +0300

    //service worker registration if ('serviceWorker' in navigator) {   window.addEventListener('load', function() {     navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {       console.log('ServiceWorker registration successful with scope: ', registration.scope);     }, function(err) {       console.log('ServiceWorker registration failed: ', err);     });   }); }

--- js/main.js
+++ js/main.js
@@ -200,16 +200,7 @@ coursesIdentifier_select.addEventListener('change', (e) => {
 hide(document.getElementById('spinner'));
 
 
-//service worker registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+
 
 //deferred events for sw
 let prompted = false
